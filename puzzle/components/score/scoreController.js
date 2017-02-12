e8e8ed25abb74c79d615d3ac640af5faea9eaaa8
Mona Elsayed <moon.gamal@gmail.com>
2017-02-12 19:11:26 +0100

    UI Fixes

--- puzzle/components/score/scoreController.js
+++ puzzle/components/score/scoreController.js
@@ -1,18 +1,28 @@
 puzzleApp.controller('scoreController', scoreController);
 
-scoreController.$inject = ['$scope', 'scoreService', '$location'];
+scoreController.$inject = ['$scope', 'scoreService', '$location', '$interval'];
 
-function scoreController($scope, scoreService, $location) {
+function scoreController($scope, scoreService, $location, $interval) {
 
     $scope.solvedWordsArr = scoreService.solvedWordsArr;
     $scope.score = scoreService.score;
+    $scope.scoreInSec = 0;
 
     $scope.play = play;
 
     if (scoreService.score && !scoreService.score.hasOwnProperty('userScore')) {
         $location.path('puzzle');
     }
-
+    else
+    {
+        if($scope.score.userScore)
+        {
+            $interval(function () {
+                $scope.scoreInSec += 1;
+            }, 100,  $scope.score.userScore);
+        }
+    }
+     
     function play() {
         $location.path('puzzle');
     }
